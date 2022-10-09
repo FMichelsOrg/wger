8451e136c8ab16b4d4ea6c8bf9fb4eaa62ef2e78
Fabian Michels <f.michels@hotmail.de>
2022-10-09 14:48:37 +0200

    Uncomment debug option

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ import sys
 from django.core.management import execute_from_command_line
 
 # Third Party
-import debugpy
+#import debugpy
 
 # wger
 from wger.tasks import (
@@ -23,11 +23,11 @@ from wger.tasks import (
 # RUN_MAIN envvar is set by the reloader to indicate that this is the 
 # actual thread running Django. This code is in the parent process and
 # initializes the debugger
-if not os.getenv("RUN_MAIN"):
-    debugpy.listen(("0.0.0.0", 9999))
-    sys.stdout.write("Start the VS Code debugger now, waiting...\n")
-    debugpy.wait_for_client()
-    sys.stdout.write("Debugger attached, starting server...\n")
+#if not os.getenv("RUN_MAIN"):
+#    debugpy.listen(("0.0.0.0", 9999))
+#    sys.stdout.write("Start the VS Code debugger now, waiting...\n")
+#    debugpy.wait_for_client()
+#    sys.stdout.write("Debugger attached, starting server...\n")
 
 if __name__ == "__main__":
 
